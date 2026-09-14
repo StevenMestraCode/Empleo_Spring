11ff1a32942059bbad833474f99c77a2acac6500
Estiven Ramirez <smestram@unicartagena.edu.co>
2026-09-13 21:45:41 -0500

    Src inicial

---
+++ src/main/java/com/unicartagena/edu/co/Empleo_Spring/EmpleoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.unicartagena.edu.co.Empleo_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleoSpringApplication.class, args);
+	}
+
+}
